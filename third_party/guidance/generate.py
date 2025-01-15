9cdef1fbd9e7c71bd78b9f6e0f0d0eddb190d1c8
seokwon99 <seokwon1999@gmail.com>
2025-01-15 14:22:27 +0900

    guidance add

--- third_party/guidance/generate.py
+++ third_party/guidance/generate.py
@@ -2,7 +2,14 @@ from guidance import models, gen
 from guidance import user, system, assistant
 
 import litellm
-litellm.api_base = "http://bob.snu.vision:11434"
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Set the API base URL.")
+parser.add_argument("--api_base", type=str, required=True, help="The base URL for the API.")
+args = parser.parse_args()
+
+litellm.api_base = args.api_base
 
 from third_party.guidance.custom_completion import CustomLiteLLMCompletion, image
 
